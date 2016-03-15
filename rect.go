4f19606a6f8a4ded6cf4759dc15e9d6029372ab7
Russell Oliver <roliver8143@gmail.com>
2016-03-15 23:14:19 +1100

    Update Rectangle struct

--- rect.go
+++ rect.go
@@ -5,8 +5,11 @@ import mt "github.com/rustyoz/Mtransform"
 type Rect struct {
 	Id        string `xml:"id,attr"`
 	Width     string `xml:"width,attr"`
-	height    string `xml:"height,attr"`
+	Height    string `xml:"height,attr"`
 	Transform string `xml:"transform,attr"`
+	Style     string `xml:"style,attr"`
+	Rx        string `xml:"rx,attr"`
+	Ry        string `xml:"ry,attr"`
 
 	transform mt.Transform
 	group     *Group
